032f7b6c8ccceecc7c25805112412e784b5dbfb4
Paloma Cortes <paloma.jaworosky@gmail.com>
2025-09-05 11:41:56 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,8 +9,8 @@ with open("requirements.txt") as f:
 setup(
     name="package_name",
     version="0.0.1",
-    author="my_name",
-    author_email="my_email",
+    author="Paloma Jaworosky",
+    author_email="jaworosky@gmail.com",
     description="My short description",
     long_description=page_description,
     long_description_content_type="text/markdown",
